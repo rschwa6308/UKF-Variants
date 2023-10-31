946ee5a70e6f2301a51ad5fcea483df23cfd7c4e
Russell Schwartz <russell.w.schwartz@gmail.com>
2023-10-31 16:00:11 -0400

    Autodiff and EKF with working example

--- src/Filters.py
+++ src/Filters.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Systems import LinearSystemModel, SystemModel
+from Systems import SystemModel, LinearSystemModel, DifferentiableSystemModel
 
 
 class Filter:
@@ -19,7 +19,8 @@ class Filter:
 
 class KalmanFilter(Filter):
     """
-    Implementation of the Bayes Filter where state belief is represented by a multivariate gaussian, restricted to purely linear system models.
+    Implementation of the Bayes Filter where state belief is represented by a multivariate gaussian,
+    restricted to purely linear system models with additive zero-mean gaussian noise models.
     """
     def __init__(self, system: LinearSystemModel):
         super().__init__(system)
@@ -56,7 +57,61 @@ class KalmanFilter(Filter):
 
 
 class ExtendedKalmanFilter(Filter):
-    pass    # TODO
+    """
+    Implementation of the Bayes Filter where state belief is represented by a multivariate gaussian,
+    allowing for potentially non-linear systems, with potentially non-additive zero-mean gaussian noise models.
+
+    System model is linearized at the current state estimate, and then linear Kalman estimation is applied.
+    """
+    def __init__(self, system: DifferentiableSystemModel):
+        super().__init__(system)
+        self.mean = None
+        self.covariance = None
+    
+    def initialize(self, mean, covariance):
+        self.mean = mean
+        self.covariance = covariance
+    
+    def predict_step(self, u):
+        """
+        First, linearize the dynamics model at the current state estimate to obtain A, B, and L
+        (respectively F_x, F_u, and F_w).
+        Then, perform the Kalman predict step:
+         - µ = f(µ, u)
+         - Σ = AΣA^T + LRL^T
+        """
+        # linearize dynamics model
+        w_mean = np.zeros((self.system.dynamics_noise_dim, 1))
+        A, B, L = self.system.dynamics_jacobian(self.mean, u, w_mean)
+
+        # Kalman predict step
+        self.mean = self.system.dynamics_model(self.mean, u)
+        self.covariance = A @ self.covariance @ A.T + L @ self.system.R @ L.T
+
+    def update_step(self, z):
+        """
+        First, linearize the measurement model at the current state estimate to obtain C and M
+        (respectively H_x and H_v).
+        Then, perform the Kalman update step:
+         - µ = µ + K(z - h(µ))
+         - Σ = Σ - KCΣ
+
+        where
+         - K = ΣC^T(CΣC^T + MQM^T)^-1
+        is the so-called "Kalman gain"
+        """
+        # linearize measurement model
+        v_mean = np.zeros((self.system.measurement_noise_dim, 1))
+        C, M = self.system.measurement_jacobian(self.mean, v_mean)
+
+        # compute Kalman gain
+        K = self.covariance @ C.T @ np.linalg.inv(C @ self.covariance @ C.T + M @ self.system.Q @ M.T)
+
+        # Kalman update state
+        self.mean += K @ (z - self.system.measurement_model(self.mean))
+        self.covariance -= K @ C @ self.covariance
+
+    
 
 class UnscentedKalmanFilter(Filter):
     pass    # TODO
--- src/Systems.py
+++ src/Systems.py
@@ -10,20 +10,36 @@ class SystemModel:
     """
     An abstract class representing a system of interest, including a dynamics model and measurement model.
 
+    Constructor requires the dimensions of the following spaces:
+     - x: state space
+     - u: control space
+     - z: measurement space
+     - w: dynamics noise space (optional, defaults to same as state space)
+     - v: measurement noise space (optional, defaults to same as measurement space)
+
     Subclasses include:
      - `LinearSystemModel`
      - `DifferentiableSystemModel`
     """
-    def __init__(self, state_dim, control_dim, measurement_dim):
+    def __init__(self, state_dim, control_dim, measurement_dim, dynamics_noise_dim=None, measurement_noise_dim=None):
         self.state_dim = state_dim
         self.control_dim = control_dim
         self.measurement_dim = measurement_dim
 
+        if dynamics_noise_dim is None:
+            dynamics_noise_dim = self.state_dim
+        
+        if measurement_noise_dim is None:
+            measurement_noise_dim = self.measurement_dim
+        
+        self.dynamics_noise_dim = dynamics_noise_dim
+        self.measurement_noise_dim = measurement_noise_dim
+
     def dynamics_model(self, x: np.array, u: np.array) -> np.array:
-        "Simulate dynamics at state x with control input u"
+        "Simulate noisy dynamics at state x with control input u"
 
     def measurement_model(self, x: np.array) -> np.array:
-        "Simulate measurement at state x"
+        "Simulate noisy measurement at state x"
 
 
 class DifferentiableSystemModel(SystemModel):
@@ -37,16 +53,16 @@ class DifferentiableSystemModel(SystemModel):
      - `SymbDiffSystemModel`: jacobians provided explicitly be user
     """
 
-    def dynamics_jacobian(self, x: np.array, u: np.array) -> Tuple[np.array, np.array]:
-        "Compute jacobian of dynamics model wrt x, and wrt u. Return both in a tuple."
+    def dynamics_jacobian(self, x: np.array, u: np.array) -> Tuple[np.array, np.array, np.array]:
+        "Compute jacobian of dynamics model wrt x, wrt u, and wrt w. Return all three in a tuple."
 
-    def measurement_jacobian(self, x: np.array) -> np.array:
-        "Compute jacobian of measurement model wrt u."
+    def measurement_jacobian(self, x: np.array) -> Tuple[np.array, np.array]:
+        "Compute jacobian of measurement model wrt u and wrt v. Return both in a tuple."
 
 
 class LinearSystemModel(DifferentiableSystemModel):
     """
-    A time-invariant system model with linear dynamics, linear measurement model, and gaussian noises.
+    A time-invariant system model with linear dynamics, linear measurement model, and additive gaussian noises.
 
     Dynamics: f(x, u) = Ax + Bu + N(0, R)
     Measurement: h(x) = Cx + N(0, Q)
@@ -55,6 +71,12 @@ class LinearSystemModel(DifferentiableSystemModel):
     """
 
     def __init__(self, A: np.array, B: np.array, C: np.array, R: np.array, Q: np.array):
+        if R.shape[0] != A.shape[0]:
+            raise ValueError("Dynamics noise covariance matrix must match state dim (as determined by A.shape)!")
+
+        if Q.shape[0] != C.shape[0]:
+            raise ValueError("Measurement noise covariance matrix must match measurement dim (as determined by C.shape)!")
+
         super().__init__(A.shape[0], B.shape[1], C.shape[0])
 
         self.A = A
@@ -64,79 +86,76 @@ class LinearSystemModel(DifferentiableSystemModel):
         self.Q = Q
     
     def dynamics_model(self, x: np.array, u: np.array) -> np.array:
-        eps = np.random.multivariate_normal(np.zeros(self.state_dim), self.R).reshape(self.state_dim, 1)
-        return self.A @ x + self.B @ u + eps
+        w = np.random.multivariate_normal(np.zeros(self.state_dim), self.R).reshape((-1, 1))
+        return self.A @ x + self.B @ u + w
     
     def measurement_model(self, x: np.array) -> np.array:
-        delta = np.random.multivariate_normal(np.zeros(self.measurement_dim), self.Q, check_valid="raise")
-        return self.C @ x + delta
+        v = np.random.multivariate_normal(np.zeros(self.measurement_dim), self.Q).reshape((-1, 1))
+        return self.C @ x + v
     
     def dynamics_jacobian(self, x: np.array, u: np.array) -> Tuple[np.array, np.array]:
-        return (self.A, self.B)
+        return (self.A, self.B, self.R)
     
     def measurement_jacobian(self, x: np.array) -> np.array:
-        return self.C
+        return (self.C, self.Q)
 
 
 class AutoDiffSystemModel(DifferentiableSystemModel):
     """
-    A system model in which jacobians are computed at runtime via automatic differentiation
-    (using JAX).
+    A time-invariant system model with arbitrary dynamics, arbitrary measurement model, and potentially
+    non-additive gaussian noises (R, Q). The jacobians are computed at runtime via automatic differentiation (using JAX). 
 
-    The constructor accepts functions for the underlying dynamics and measurement models.
+    The constructor accepts functions for the underlying dynamics and measurement models. Note that (for the sake of consistency) these functions must operate on column vectors:
+     - `dynamics_func(x: (1, state_dim), u: (1, control_dim), w: (1, dynamics_noise_dim)) -> (1, state_dim)`
+     - `measurement_func(x: (1, state_dim), v: (1, measurement_noise_dim)) -> (1, measurement_dim)`
+    
+    Moreover, these functions must operate on `jax.numpy` arrays and must use the corresponding methods.
     """
 
-    def __init__(self, state_dim, control_dim, measurement_dim, dynamics_func: Callable, measurement_func: Callable):
-        super().__init__(state_dim, control_dim, measurement_dim)
+    def __init__(self, state_dim, control_dim, measurement_dim, dynamics_func: Callable, measurement_func: Callable, R: np.array, Q: np.array):
+        super().__init__(state_dim, control_dim, measurement_dim, R.shape[0], Q.shape[0])
 
         self.dynamics_func = dynamics_func
         self.measurement_func = measurement_func
 
-        self.dynamics_func_dx = jax.grad()      # TODO
-        self.dynamics_func_du = jax.grad()      # TODO
+        self.R = R
+        self.Q = Q
 
-        self.measurement_func_dx = jax.grad()   # TODO
+        self.dynamics_func_dx = jax.jacfwd(self.dynamics_func, argnums=0)       # wrt first arg (x)
+        self.dynamics_func_du = jax.jacfwd(self.dynamics_func, argnums=1)       # wrt second arg (u)
+        self.dynamics_func_dw = jax.jacfwd(self.dynamics_func, argnums=2)       # wrt second arg (w)
 
-    def dynamics_jacobian(self, x: np.array, u: np.array) -> Tuple[np.array, np.array]:
-        F_x = self.dynamics_func_dx(x, u)
-        F_u = self.dynamics_func_du(x, u)
+        self.measurement_func_dx = jax.jacfwd(self.measurement_func, argnums=0) # wrt first arg (x)
+        self.measurement_func_dv = jax.jacfwd(self.measurement_func, argnums=1) # wrt second arg (v)
 
-        return (F_x, F_u)
+    def dynamics_model(self, x: np.array, u: np.array) -> np.array:
+        w = np.random.multivariate_normal(np.zeros(self.dynamics_noise_dim), self.R).reshape((-1, 1))
+        return self.dynamics_func(x, u, w)
     
-    def measurement_jacobian(self, x: np.array) -> np.array:
-        H_x = self.measurement_func_dx(x)
+    def measurement_model(self, x: np.array) -> np.array:
+        v = np.random.multivariate_normal(np.zeros(self.measurement_noise_dim), self.Q).reshape((-1, 1))
+        return self.measurement_func(x, v)
 
-        return H_x
+    def dynamics_jacobian(self, x: np.array, u: np.array, w: np.array) -> Tuple[np.array, np.array]:
+        # evaluate jacobians (JAX requires explicit floats)
+        F_x = self.dynamics_func_dx(x.astype(float), u.astype(float), w.astype(float))
+        F_u = self.dynamics_func_du(x.astype(float), u.astype(float), w.astype(float))
+        F_w = self.dynamics_func_dw(x.astype(float), u.astype(float), w.astype(float))
 
+        # remove extra column-vector dimensions
+        F_x = F_x.reshape(self.state_dim, self.state_dim)
+        F_u = F_u.reshape(self.state_dim, self.control_dim)
+        F_w = F_w.reshape(self.state_dim, self.dynamics_noise_dim)
 
+        return (F_x, F_u, F_w)
 
-class DifferentiableSystemModel(SystemModel):
-    def __init__(self):
-        super().__init__(3, 3, 3) # Assuming placeholder dimension values until reasoned from f and h
+    def measurement_jacobian(self, x: np.array, v: np.array) -> np.array:
+        # evaluate jacobian (JAX requires explicit floats)
+        H_x = self.measurement_func_dx(x.astype(float), v.astype(float))
+        H_v = self.measurement_func_dv(x.astype(float), v.astype(float))
 
-    def f(x1,x2):
-        return (x2**2,x1**2)
-    
-    def h(x1,x2):
-        return (x2**3,x1**3)
-    
-    def dynamics_jacobian(self,x: np.array, u:np.array):
-        futureTuple = []
-        for i in range(0,x.size):
-            futureTuple.append(torch.tensor(float(x[i])))
+        # remove extra column-vector dimensions
+        H_x = H_x.reshape((self.measurement_dim, self.state_dim))
+        H_v = H_v.reshape((self.measurement_dim, self.measurement_noise_dim))
 
-        myNewTuple = tuple(futureTuple)
-        #return jacobian(self.f,myNewTuple)
-        return self.jacobian_to_np_array(torch.autograd.functional.jacobian(self.f,myNewTuple))    # TODO
-
-    def measurement_jacobian(x):
-        pass    # TODO
-
-    def jacobian_to_np_array(self,jacobian):
-        array = np.zeros((self.state_dim,self.state_dim))
-
-        for i in range(0,self.state_dim**2):
-            local_tuple = jacobian[i%3]
-            array[i//3,i%3] = float(local_tuple[i//3])
-
-        return array
+        return (H_x, H_v)
