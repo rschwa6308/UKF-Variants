7c68b14b16e4fb646481104da0e4a58f958c6052
Russell Schwartz <russell.w.schwartz@gmail.com>
2023-11-01 11:44:51 -0400

    add labels and improve vis

--- src/visualizations.py
+++ src/visualizations.py
@@ -18,7 +18,7 @@ def plot_pdf(ax: plt.Axes, domain: np.array, pdf_values: np.array, transpose_axi
         ax.set_xlim(0, None)
 
 
-def plot_pdf_transform(domain, prior, transform_values, posterior, slice_highlights=[], highlight_color="tab:orange", transform_label=None, title=None):
+def plot_pdf_transform(domain, prior, transform_values, posterior, slice_highlights=[], highlight_color="tab:orange", prior_label=None, posterior_label=None, transform_label=None, title=None):
     fig, axes = plt.subplots(
         2, 2, figsize=(6, 6),
         sharex="col", sharey="row",
@@ -27,14 +27,14 @@ def plot_pdf_transform(domain, prior, transform_values, posterior, slice_highlig
     axes[1, 0].axis("off")
 
     # plot PDFs
-    plot_pdf(axes[1, 1], domain, prior)
-    plot_pdf(axes[0, 0], domain, posterior, transpose_axis=True)
+    plot_pdf(axes[1, 1], domain, prior, label=prior_label, zorder=2)
+    plot_pdf(axes[0, 0], domain, posterior, transpose_axis=True, label=posterior_label, zorder=2)
 
     # plot transform
     axes[0, 1].plot(domain, transform_values, label=transform_label)
 
     # set PDF axis limits explicitly so they match
-    pmax = max(np.max(prior), np.max(posterior)) * 1.1
+    pmax = max(np.max(prior), np.max(posterior)) * 1.2
     axes[0, 0].set_xlim(0, pmax)
     axes[1, 1].set_ylim(0, pmax)
 
@@ -53,9 +53,11 @@ def plot_pdf_transform(domain, prior, transform_values, posterior, slice_highlig
     if title is not None:
         fig.suptitle(title)
 
-    # optional: make a legend for the transform
-    if transform_label is not None:
-        axes[0, 1].legend()
+    # optional: make legends
+    if prior_label is not None:     axes[1, 1].legend(loc="upper right")
+    if posterior_label is not None: axes[0, 0].legend(loc="upper right")
+    if transform_label is not None: axes[0, 1].legend(loc="upper right")
+    
 
     # for given slices of the domain, highlight the preimage and image under the transform
     for x_low, x_high in slice_highlights:
@@ -63,45 +65,56 @@ def plot_pdf_transform(domain, prior, transform_values, posterior, slice_highlig
         x_low_idx = int((x_low - domain[0]) / domain_step)
         x_high_idx = int((x_high - domain[0]) / domain_step)
 
-        prior_low = query_pdf(domain, prior, x_low)
-        prior_high = query_pdf(domain, prior, x_high)
-
         y_low = transform_values[x_low_idx]
         y_high = transform_values[x_high_idx]
 
-        posterior_low = query_pdf(domain, posterior, y_low)
-        posterior_high = query_pdf(domain, posterior, y_high)
-
-        fig.add_artist(ConnectionPatch(
-            (x_low, prior_low), (x_low, y_low),
+        axes[1,1].add_artist(ConnectionPatch(
+            (x_low, 0), (x_low, y_low),
             coordsA="data", coordsB="data",
             axesA=axes[1, 1], axesB=axes[0, 1],
-            color=highlight_color, linestyle="--", linewidth=0.8
+            color=highlight_color, linestyle="--", linewidth=0.8,
+            zorder=1
         ))
 
-        fig.add_artist(ConnectionPatch(
-            (x_high, prior_high), (x_high, y_high),
+        axes[1,1].add_artist(ConnectionPatch(
+            (x_high, 0), (x_high, y_high),
             coordsA="data", coordsB="data",
             axesA=axes[1, 1], axesB=axes[0, 1],
-            color=highlight_color, linestyle="--", linewidth=0.8
+            color=highlight_color, linestyle="--", linewidth=0.8,
+            zorder=1
         ))
 
-        fig.add_artist(ConnectionPatch(
-            (posterior_low, y_low), (x_low, y_low),
+        axes[0,0].add_artist(ConnectionPatch(
+            (0, y_low), (x_low, y_low),
             coordsA="data", coordsB="data",
             axesA=axes[0, 0], axesB=axes[0, 1],
-            color=highlight_color, linestyle="--", linewidth=0.8
+            color=highlight_color, linestyle="--", linewidth=0.8,
+            zorder=1
         ))
 
-        fig.add_artist(ConnectionPatch(
-            (posterior_high, y_high), (x_high, y_high),
+        axes[0,0].add_artist(ConnectionPatch(
+            (0, y_high), (x_high, y_high),
             coordsA="data", coordsB="data",
             axesA=axes[0, 0], axesB=axes[0, 1],
-            color=highlight_color, linestyle="--", linewidth=0.8
+            color=highlight_color, linestyle="--", linewidth=0.8,
+            zorder=1
         ))
 
         mask = (x_low <= domain) & (domain <= x_high)
-        plot_pdf(axes[1, 1], domain[mask], prior[mask[:-1]][:-1], linestyle="--", alpha=0.9, linewidth=0.8, facecolor=highlight_color)
+        plot_pdf(
+            axes[1, 1], domain[mask], prior[mask[:-1]][:-1],
+            linestyle="--", alpha=0.9, linewidth=0.8, facecolor=highlight_color,
+            zorder=2
+        )
 
         mask = (y_low <= domain) & (domain <= y_high)
-        plot_pdf(axes[0, 0], domain[mask], posterior[mask[:-1]][:-1], transpose_axis=True, linestyle="--", alpha=0.9, linewidth=0.8, facecolor=highlight_color)
+        plot_pdf(
+            axes[0, 0], domain[mask], posterior[mask[:-1]][:-1], transpose_axis=True,
+            linestyle="--", alpha=0.9, linewidth=0.8, facecolor=highlight_color,
+            zorder=2
+        )
+
+    # change subplot draw order so that the connection patches draw on top of the transform plot
+    axes[0, 0].set_zorder(1)
+    axes[0, 1].set_zorder(0)
+    axes[1, 1].set_zorder(1)
